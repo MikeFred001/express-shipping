d1fb19da88e03d0ae9c79787c051c7dc7d9d969a
michael frederickson <mpfplus@gmail.com>
2023-08-15 18:35:49 -0600

    Done with first part, moving on to part 2

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const express = require("express");
+const jsonschema = require("jsonschema");
+const shipItSchema = require("../schemas/shipItSchema.json");
 const { BadRequestError } = require("../expressError");
 const router = new express.Router();
 
@@ -11,6 +13,8 @@ const { shipProduct } = require("../shipItApi");
  * VShips an order coming from json body:
  *   { productId, name, addr, zip }
  *
+ * Checks body for invalid inputs and returns errors if necessary
+ *
  * Returns { shipped: shipId }
  */
 
@@ -18,10 +22,19 @@ router.post("/", async function (req, res, next) {
   if (req.body === undefined) {
     throw new BadRequestError();
   }
+
+  console.log("SHIPMENT ROUTE BODY", req.body);
+
+  const result = jsonschema.validate(req.body, shipItSchema, {required: true});
+
+  if (!result.valid) {
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
 });
-
 
 module.exports = router;
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,5 +1,11 @@
 "use strict";
 
+// const AxiosMockAdapter = require("axios-mock-adapter");
+// const axios = require("axios");
+// const axiosMock = new AxiosMockAdapter(axios);
+// const { shipProduct } = require("../shipItApi");
+
+
 const request = require("supertest");
 const app = require("../app");
 
@@ -10,9 +16,9 @@ describe("POST /", function () {
       productId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
-      zip: "12345-6789",
+      zip: "12345-6789"
     });
-
+    console.log("RESPONSE BODY>>>>>", resp.body);
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
 
@@ -22,4 +28,43 @@ describe("POST /", function () {
       .send();
     expect(resp.statusCode).toEqual(400);
   });
+
+  test("throws error(s) if invalid request body", async function () {
+
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        productId: 999,
+        name: 90,
+        addr: "123 Fake St.",
+        zip: "88888"
+      });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      error: {
+        message: [
+          "instance.productId must be greater than or equal to 1000",
+          "instance.name is not of a type(s) string"
+        ], status: 400 }});
+  });
 });
+
+
+// async function shipProduct({ productId, name, addr, zip }) {
+//   console.warn("Called our real shipProduct function");
+
+//   const resp = await axios({
+//     method: "POST",
+//     url: SHIPIT_SHIP_URL,
+//     data: {
+//       itemId: productId,
+//       name: name,
+//       addr: addr,
+//       zip: zip,
+//       key: SHIPIT_API_KEY
+//     },
+//   });
+
+//   return resp.data.receipt.shipId;
+// }
